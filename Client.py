73a534037c0aba34aed0c134040647edea1a8e56
EnzoLindauer <enzo.lindauer@lau.edu>
2025-04-14 18:17:58 +0300

    ChoiceMenu

--- Client.py
+++ Client.py
@@ -31,12 +31,10 @@ MANY THREADS: FOR SERVICE HANDLING
 
 # def uploadFiles(...):
 
-
 # def etc etc():
 # - List available Files
 # - Download File
 # - Upload File
-
 
 # def fileIntegrityCheck(...):
 
--- Server.py
+++ Server.py
@@ -14,7 +14,6 @@ MAIN THREAD: FOR CONNECTION INITIATION
 from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
-
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 
