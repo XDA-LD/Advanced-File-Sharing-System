546b4939ae517d69ae80fda36477ef4c360b8bd5
XDA-LD <luiji.daou2@gmail.com>
2025-04-14 09:42:26 -0400

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,37 @@
+
+
+def sendChoice():
+    try:
+        send(bytes)
+    catch:
+
+
+
+"""
+------------------------------------
+MAIN THREAD: FOR GUI
+------------------------------------
+"""
+
+
+DO GUI STUFF AND CALL NON-GUI METHODS
+
+
+"""
+------------------------------------
+MANY THREADS: FOR SERVICE HANDLING
+------------------------------------
+"""
+
+def uploadFiles(...):
+
+
+def etc etc():
+- List available Files
+- Download File
+- Upload File
+
+
+def fileIntegrityCheck(...):
+
+
---
+++ Server.py
@@ -0,0 +1,60 @@
+import db_handler
+total_logs = ""
+
+
+
+
+"""
+------------------------------------
+MAIN THREAD: FOR CONNECTION INITIATION
+------------------------------------
+"""
+
+
+while TRUE:
+    MAKE CONNECTION
+    CREATE A MENULOOP THREAD
+
+
+
+"""
+------------------------------------
+MANY THREADS: FOR SERVICE HANDLING
+------------------------------------
+"""
+
+session_key = None
+
+```python
+While True:
+    choice = receive(...)
+    if choice == "UploadFiles":
+        UploadFiles(...)
+    ...
+    elif choice == "Case i":
+    caseI(...)
+    ...
+    else:
+    closeConnection(...)
+    break;
+```
+
+"""
+------------------------------------
+HELPER METHODS
+------------------------------------
+"""
+
+def uploadFiles(...):
+
+
+def etc etc():
+- Menu
+- List available Files
+- Download File
+- Upload File
+
+
+def fileIntegrityCheck(...):
+
+
