3bbb993f145ab6789801f03aa405103edd7992f2
OlexandrGhanem <olexandrghanem@gmail.com>
2025-04-14 17:23:30 +0300

    Update db_handler.py

--- db_handler.py
+++ db_handler.py
@@ -1,3 +1,6 @@
+import mysql.connector
+import pymysql
+from mysql.connector import Error
 
 def create_connection():
     connection = None
@@ -6,7 +9,7 @@ def create_connection():
             host='127.0.0.1',
             user='root',
             password='gasanchik',
-            database='assDB'
+            database='Storage'
         )
         print("Connection to MySQL DB successful")
     except Error as e:
@@ -14,20 +17,31 @@ def create_connection():
 
     return connection
 
-```python
-def createAccount(name, pwd):
-	if notAlreadyInDB(...)
-		SQLCreateAccount(...)
-		return True
-	else:
-		return False
-```
+def isAdmin(connection:pymysql.connections.Connection,userName, passWord):
+    # returns boolean , true: admin, false: normal user
+    with connection.cursor() as cursor:
+        sql = "SELECT is_admin FROM Users WHERE user_name = %s and user_password = %s"
+        cursor.execute(sql, (userName,passWord))
+        result = cursor.fetchone()
+        return result
 
-returns role if logged in, "none" if not
-```python
-def isAdmin(Name, Pwd):
-	return queryRole(Name, Pwd)
-```
+def userExists(connection:pymysql.connections.Connection,userName, passWord):
+    #checks if user is in Database
+    with connection.cursor() as cursor:
+        sql = "SELECT EXISTS( SELECT * FROM Users WHERE user_name = %s and user_password = %s)"
+        cursor.execute(sql,(userName,passWord))
+        result = cursor.fetchone()
+        return result
+
+def userCreateAccount(connection:pymysql.connections.Connection,userName, passWord):
+    #creates account if user doesnt exist
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO Users(user_name, user_password,is_admin) VALUES (%s,%s,%s)"
+        cursor.execute(sql,(userName, passWord, "FALSE"))
+        return
+
+
+
 
 ```python
 def uploadFile(socket):
