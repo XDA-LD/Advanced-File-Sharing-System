2e031aa4f45866d0279333d3432b339b237daf8e
OlexandrGhanem <olexandrghanem@gmail.com>
2025-04-16 17:52:58 +0300

    Update initDatabase.sql

--- initDatabase.sql
+++ initDatabase.sql
@@ -11,7 +11,7 @@ PRIMARY KEY (user_name, user_password)
 
 CREATE TABLE Files(
 file_name varchar(100),
-file_fullname varchar(100),
+file_path varchar(150),
 file_version int DEFAULT 0,
 file_checksum_type varchar(20) NOT NULL,
 file_checksum_value varchar(256) NOT NULL,
