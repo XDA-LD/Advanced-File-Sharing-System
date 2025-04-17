25c8f76393a426886637ff6c96a18a16b8e8017f
OlexandrGhanem <olexandrghanem@gmail.com>
2025-04-17 19:16:49 +0300

    Update Logger.py

--- Logger.py
+++ Logger.py
@@ -1,12 +1,14 @@
 from datetime import datetime
+import threading
+
+lock = threading.Lock()
 
 
 def log(file_name, user_name, action):
     # action can be uploaded, deleted, downloaded
-    curr_time = datetime.now()
-    message = f"File {file_name} {action} by user {user_name} at time: {curr_time}"
-    file = open("Log.txt", "a")
-    file.write(message)
-    file.close()
-
-
+    with lock:
+        curr_time = datetime.now()
+        file = open("Log.txt", "a")
+        message = f"File {file_name} {action} by user {user_name} at time: {curr_time}"
+        file.write(message)
+        file.close()
